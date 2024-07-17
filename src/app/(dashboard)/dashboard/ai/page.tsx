6340b3b62d5167f8b0405a89f53dac3ae120ec08
Atakan Atik <atknatk@gmail.com>
2024-07-17 20:48:52 +0000

    updated

--- src/app/(dashboard)/dashboard/ai/page.tsx
+++ src/app/(dashboard)/dashboard/ai/page.tsx
@@ -1,15 +1,17 @@
 import { AI } from '@/app/actions'
 import { Chat } from '@/components/ai/chat'
 import { nanoid } from 'ai'
-
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 export const maxDuration = 60
 
 export default function Page() {
   const id = nanoid()
   return (
+    <ScrollArea className="h-full">
     <AI initialAIState={{ chatId: id, messages: [] }}>
       <Chat id={id} />
     </AI>
+    </ScrollArea>
   )
 }

7b4b113b1af45d9627b5b9858205fbb07a5041fb
Atakan Atik <atknatk@gmail.com>
2024-07-17 12:01:11 +0000

    updated

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -3,6 +3,7 @@ import CredentialProvider from 'next-auth/providers/credentials';
 import GithubProvider from 'next-auth/providers/github';
 
 const authConfig = {
+  trustHost: true,
   providers: [
     // GithubProvider({
     //   clientId: process.env.GITHUB_ID ?? '',
